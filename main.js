d0320d2f8d65ce20f59647e6f0aa6da8b15ae17b
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-01-15 12:56:04 +0530

    Add files via upload
    App completed.

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 }
 
+function modelLoaded(){
+  console.log('MobileNet model Loaded');
+}
 
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
 
+var previous_result = '';
+
+function gotResult(error, result){
+if(error){
+  console.log(error);
+} else{
+  if((result[0].confidence > 0.5) && (previous_result = result[0].label)){
+    console.log(result);
+    previous_result = result[0].label;
+    /*var synth = window.speechSynthesis;
+    speak_data = 'Object detected is - '+result[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);*/
+
+    document.getElementById("result_object_name").innerHTML = result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);
+  }
+}
+}
+
+function speech_enabled(){
+  var synth = window.speechSynthesis;
+  speak_data = 'Object detected is - '+document.getElementById("result_object_name").innerHTML;
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterThis);
+
+}
